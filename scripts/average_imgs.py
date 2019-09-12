a7b2956e88852e7ed1eb322b065b966b54e11321
Jonathan Tay <jonathantay193@gmail.com>
2019-09-12 21:29:26 +0100

    corrected loop and added flipping in script

--- scripts/average_imgs.py
+++ scripts/average_imgs.py
@@ -10,10 +10,12 @@ img_list = [os.path.join(fpath, x) for x in img_list]
 example_img = nib.load(img_list[0])
 template = np.zeros(example_img.get_data().shape)
 
-for i in os.listdir(fpath):
+for i in img_list:
     template += nib.load(i).get_data()
 
 n_imgs = len(img_list)
 template = template / n_imgs
+template_flipped = np.fliplr(template)
+template = (template + template_flipped) / 2
 template_img = nib.Nifti1Image(template, example_img.affine)
 nib.save(template_img, os.path.join(fpath, f'template_{n_imgs}.nii.gz'))
