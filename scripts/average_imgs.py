0bf62503414796cc7140b60122f9adc309b2d457
Jonathan Tay <jonathantay193@gmail.com>
2019-09-12 21:11:45 +0100

    finished script testing affine

--- scripts/average_imgs.py
+++ scripts/average_imgs.py
@@ -6,6 +6,13 @@ import nibabel as nib
 fpath = sys.argv[1]
 img_list = os.listdir(fpath)
 img_list = [os.path.join(fpath, x) for x in img_list]
-print(img_list)
+
+example_img = nib.load(img_list[0])
+template = np.zeros(example_img.get_data().shape)
+print(example_img.affine)
 
 # for i in os.listdir(fpath):
+#     template += nib.load(i).get_data()
+
+# template = template / len(img_list)
+# template_img = nib.Nifti1Image(template, example_img.affine)
